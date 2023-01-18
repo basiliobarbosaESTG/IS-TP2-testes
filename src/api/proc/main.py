9990cec6d7555ffb6b48073113322996d56bebd9
basiliobarbosaESTG <basiliobarbosa@ipvc.pt>
2023-01-18 01:38:12 +0000

    versao entrega dia 18

--- src/api/proc/main.py
+++ src/api/proc/main.py
@@ -1,11 +1,14 @@
 import sys
+import psycopg2
 
 from flask import Flask
+from flask_cors import CORS
 
 PORT = int(sys.argv[1]) if len(sys.argv) >= 2 else 9000
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app)
 
 
 @app.route('/api/best_players', methods=['GET'])
@@ -20,105 +23,106 @@ def get_best_players():
     }]
 
 
-def getAthleteByName(name, id):  # recebe como parametros: o id e o nome
-    try:
-        cursor.execute("SELECT unnest(CAST(XPATH('/athletes/atlethe[@name=\""+name+"\"]/@name', xml)AS TEXT)::text[]) AS nome, unnest(CAST(xpath('/athletes/atlethe[@name=\""+name +
-                       "\"]/sex/text()', xml)AS TEXT)::text[]) AS sexo, unnest(CAST(xpath('/athletes/atlethe[@name=\""+name+"\"]/age/text()', xml)AS TEXT)::text[]) AS idade FROM xmldata where id="+id)
-        # cursor.execute("SELECT unnest(xpath('/athletes/atlethe[@name=\""+name+"\"]/sex/text()', xml)) AS sexo FROM xmldata where id="+id)
-        connection.commit()
-        result = cursor.fetchall()
-        print(len(result))
-        # print(type(result[0]))
-        print(type(result))
-        # Type serve para ver o tipo de dado da variavel
-        return result
-    except (Exception, Error) as error:
-        connection.rollback()
-        print("Erro de execução: ", error)
-        return (str(error))
+@app.route('/api/athleteByName', methods=['GET'])
+def getAthleteByName(name, id):
+    connection = psycopg2.connect(
+        host='db-rel2', database='is', user='is', password='is')
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT unnest(CAST(XPATH('/athletes/atlethe[@name=\""+name+"\"]/@name', xml)AS TEXT)::text[]) AS nome, unnest(CAST(xpath('/athletes/atlethe[@name=\""+name +
+                   "\"]/sex/text()', xml)AS TEXT)::text[]) AS sexo, unnest(CAST(xpath('/athletes/atlethe[@name=\""+name+"\"]/age/text()', xml)AS TEXT)::text[]) AS idade FROM xmldata where id="+id)
+    connection.commit()
+    data = cursor.fetchall()
+
+    print(len(data))
+    print(type(data))
+    return data
+
 
 # Consulta que conta quantas medalhas existem de cada tipo
-
-
+@app.route('/api/groupByMedals', methods=['GET'])
 def getGroupByMedals(id):
-    try:
-        cursor.execute("SELECT unnest(cast(xpath('/athletes/atlethe/competition/statsBySport/medal/text()', xml)as TEXT)::text[]) as medalhas, count(*) as contagem FROM xmldata where id="+id+" group by medalhas")
-        connection.commit()
-        result = cursor.fetchall()
-        print(len(result))
-        print(type(result))
-        return result
-    except (Exception, Error) as error:
-        connection.rollback()
-        print("Erro de execução: ", error)
-        return (str(error))
+    connection = psycopg2.connect(
+        host='db-rel2', database='is', user='is', password='is')
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT unnest(cast(xpath('/athletes/atlethe/competition/statsBySport/medal/text()', xml)as TEXT)::text[]) as medalhas, count(*) as contagem FROM xmldata where id="+id+" group by medalhas")
+    connection.commit()
+    data = cursor.fetchall()
+
+    print(len(data))
+    print(type(data))
+    return data
 
 # Consulta que conta quantos tipos de desportos existem
 
 
+@app.route('/api/groupBySport', methods=['GET'])
 def getGroupBySport(id):
-    try:
-        cursor.execute("SELECT unnest(cast(xpath('/athletes/atlethe/competition/statsBySport/sport/text()', xml)as TEXT)::text[]) as desporto, count(*) as contagem FROM xmldata where id="+id+" group by desporto")
-        connection.commit()
-        result = cursor.fetchall()
-        print(len(result))
-        print(type(result))
-        return result
-    except (Exception, Error) as error:
-        connection.rollback()
-        print("Erro de execução: ", error)
-        return (str(error))
+    connection = psycopg2.connect(
+        host='db-rel2', database='is', user='is', password='is')
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT unnest(cast(xpath('/athletes/atlethe/competition/statsBySport/sport/text()', xml)as TEXT)::text[]) as desporto, count(*) as contagem FROM xmldata where id="+id+" group by desporto")
+    connection.commit()
+    data = cursor.fetchall()
+
+    print(len(data))
+    print(type(data))
+    return data
+
 
 # Consulta que conta quantos atletas existem com aquele tipo de sexo(NESTE CASO CONTA QUANTOS ATLETAS EXISTEM DO SEXO MASCULINO E FEMININO)
-
-
+@app.route('/api/groupBySex', methods=['GET'])
 def getGroupBySex(id):
-    try:
-        cursor.execute("SELECT unnest(cast(xpath('/athletes/atlethe/sex/text()', xml)as TEXT)::text[]) as sex, count(*) as contagem FROM xmldata where id=" +
-                       id+" group by sex order by contagem asc")
-        connection.commit()
-        result = cursor.fetchall()
-        print(len(result))
-        print(type(result))
-        return result
-    except (Exception, Error) as error:
-        connection.rollback()
-        print("Erro de execução: ", error)
-        return (str(error))
+    connection = psycopg2.connect(
+        host='db-rel2', database='is', user='is', password='is')
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT unnest(cast(xpath('/athletes/atlethe/sex/text()', xml)as TEXT)::text[]) as sex, count(*) as contagem FROM xmldata where id=" +
+                   id+" group by sex order by contagem asc")
+    connection.commit()
+    data = cursor.fetchall()
+
+    print(len(data))
+    print(type(data))
+    return data
 
 # Consulta que apresenta o tipo de desporto praticado através do nome de um determinado atleta
 
 
+@app.route('/api/sportByName', methods=['GET'])
 def getSportByName(name, id):
-    try:
-        cursor.execute(
-            "SELECT unnest(cast(xpath('/athletes/atlethe[@name=\""+name+"\"]/competition/statsBySport/sport/text()', xml)as TEXT)::text[]) as desporto FROM xmldata where id="+id)
-        connection.commit()
-        result = cursor.fetchall()
-        print(len(result))
-        print(type(result))
-        return result
-    except (Exception, Error) as error:
-        connection.rollback()
-        print("Erro de execução: ", error)
-        return (str(error))
+    connection = psycopg2.connect(
+        host='db-rel2', database='is', user='is', password='is')
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "SELECT unnest(cast(xpath('/athletes/atlethe[@name=\""+name+"\"]/competition/statsBySport/sport/text()', xml)as TEXT)::text[]) as desporto FROM xmldata where id="+id)
+    connection.commit()
+    data = cursor.fetchall()
+
+    print(len(data))
+    print(type(data))
+    return data
 
 # Consulta que apresenta o tipo de evento desportivo praticado através do nome de um determinado atleta
 
 
+@app.route('/api/eventByName', methods=['GET'])
 def getEventByName(name, id):
-    try:
-        cursor.execute(
-            "SELECT unnest(cast(xpath('/athletes/atlethe[@name=\""+name+"\"]/competition/statsBySport/event/text()', xml)as TEXT)::text[]) as evento FROM xmldata where id="+id)
-        connection.commit()
-        result = cursor.fetchall()
-        print(len(result))
-        print(type(result))
-        return result
-    except (Exception, Error) as error:
-        connection.rollback()
-        print("Erro de execução: ", error)
-        return (str(error))
+    connection = psycopg2.connect(
+        host='db-rel2', database='is', user='is', password='is')
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "SELECT unnest(cast(xpath('/athletes/atlethe[@name=\""+name+"\"]/competition/statsBySport/event/text()', xml)as TEXT)::text[]) as evento FROM xmldata where id="+id)
+    connection.commit()
+    data = cursor.fetchall()
+
+    print(len(data))
+    print(type(data))
+    return data
 
 
 if __name__ == '__main__':
